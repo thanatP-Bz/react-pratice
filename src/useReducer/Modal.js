dee0d0a5d502cd7e92cdd697d866ef8e72951cac
Thanat <bztzn512@gmail.com>
2021-11-18 21:18:34 -0500

    finalUseReducer

--- src/useReducer/Modal.js
+++ src/useReducer/Modal.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Modal = ({ showContent }) => {
+const Modal = ({ showContent, closeModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      closeModal();
+    }, 3000);
+  });
+
   return (
     <div className="modal">
       <h4>{showContent}</h4>
--- src/useReducer/index.js
+++ src/useReducer/index.js
@@ -1,63 +1,102 @@
 import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
-import Data from "../Data";
 
 const reducer = (state, action) => {
   /* return state */
-  if (action.type === "TESTING") {
+  if (action.type === "ADD_ITEM") {
     /* must return object */
+    const newItems = [...state.people, action.payload];
     return {
       ...state,
-      people: Data,
+      people: newItems,
       isShowModal: true,
-      showContent: "testing",
+      showContent: "ITEM ADDED",
     };
+  }
+  if (action.type === "NO_VALUE") {
+    return { ...state, isShowModal: true, showContent: "please enter value" };
+  }
+
+  if (action.type === "CLOSE_MODAL") {
+    return { ...state, isShowModal: false };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    const newPeople = state.people.filter(
+      (person) => person.id !== action.payload
+    );
+    return { ...state, people: newPeople };
   }
   /*  return state; */ /* or we can throw error */
   throw new Error("no matching Value");
 };
 
 const initState = {
-  people: [Data],
+  people: [],
   isShowModal: false,
   showContent: "hello",
 };
 
 const Index = () => {
   const [state, dispatch] = useReducer(reducer, initState);
-
+  const [age, setAge] = useState("");
   const [name, setName] = useState("");
 
   const onFormSubmit = (e) => {
     e.preventDefault();
 
-    if (name) {
-      dispatch({ type: "ADD_ITEM" });
+    if (name && age) {
+      const newItems = {
+        id: Math.random().toString(),
+        name,
+        age,
+      };
+      dispatch({ type: "ADD_ITEM", payload: newItems });
+      setName("");
+      setAge("");
     } else {
-      dispatch({ type: "RANDOM" });
+      dispatch({ type: "NO_VALUE" });
     }
+  };
+
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
   };
 
   const onChangeName = (e) => {
     setName(e.target.value);
-    console.log(e.target.value);
+  };
+
+  const onChangeAge = (e) => {
+    setAge(e.target.value);
   };
 
   return (
     <>
-      {state.isShowModal && <Modal showContent={state.showContent} />}
+      {state.isShowModal && (
+        <Modal showContent={state.showContent} closeModal={closeModal} />
+      )}
       <form onSubmit={onFormSubmit} className="form">
         <label htmlFor="firstName">Name</label>
         <input type="text" value={name} onChange={onChangeName} />
-
+        <label htmlFor="age">Age</label>
+        <input type="text" value={age} onChange={onChangeAge} />
         <button className="btn" type="submit">
           add person
         </button>
       </form>
       {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className="item">
             <h4>{person.name}</h4>
+            <h4>{person.age}</h4>
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: person.id })
+              }
+            >
+              remove
+            </button>
           </div>
         );
       })}

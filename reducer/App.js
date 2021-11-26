9000a5ffc2a30657197f42bc7f72387a245ce29c
Thanat <bztzn512@gmail.com>
2021-11-25 19:38:46 -0500

    axios

---
+++ reducer/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Index from "./Practice/Index";
+
+function App() {
+  return (
+    <div>
+      <Index />
+    </div>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from "react";
-import Index from "./Practice/Index";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const request = await axios.get(url);
+    setLoading(false);
+    setJobs(request.data);
+    console.log(request.data);
+    return request;
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, [url]);
+
+  if (loading) {
+    return <section>Loading...</section>;
+  }
+
+  const { dates, company } = jobs[value];
+
   return (
     <div>
-      <Index />
+      <h3>{dates}</h3>
+      <p>{company}</p>
     </div>
   );
 }

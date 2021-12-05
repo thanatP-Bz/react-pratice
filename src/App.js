222e758209636955f0fa535352ab5e0ccb712542
Thanat <bztzn512@gmail.com>
2021-12-04 21:30:12 -0500

    sliderStater

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import data from "./data";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
   return (
-    <div>
+    <div className="container">
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, title, desc } = person;
 
+          return (
+            <div key={id}>
+              <h4>{title}</h4>
+              <p>{desc}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const people = [
+  {
+    id: 1,
+    title: "cat",
+    desc: "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+  },
+  {
+    id: 2,
+    title: "geraffe",
+    desc: "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+  },
+  {
+    id: 3,
+    title: "bird",
+    desc: "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+  },
+  {
+    id: 4,
+    title: "dog",
+    desc: "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+  },
+];
+
+export default people;

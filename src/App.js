74d3b6d42705a382bed31a34427be76c6cf6fdbf
Thanat <bztzn512@gmail.com>
2021-12-07 21:15:22 -0500

    slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import data from "./data";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
   return (
     <div className="container">
       <div className="section-center">
@@ -12,12 +23,15 @@ function App() {
           const { id, title, desc } = person;
 
           let position = "nextSlide";
-
           if (personIndex === index) {
             position = "activeSlide";
           }
-
-          if() {}
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
 
           return (
             <article key={id} className={position}>
@@ -26,10 +40,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex((index) => index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex((index) => index + 1)}>
           <FiChevronRight />
         </button>
       </div>

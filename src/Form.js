81342925ed64276f8528143d580e0de197317904
Thanat <bztzn512@gmail.com>
2022-05-13 22:10:35 -0400

    pt

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 const Form = () => {
+  const [firstName, setFirstName] = useState("");
+  const [password, setPassword] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [people, setPeople] = useState([]);
+
   const submitHandler = (e) => {
     e.preventDefault();
   };
@@ -10,13 +16,16 @@ const Form = () => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor="">name</label>
-      <input type="text" name="name" onChange={onChangeHandler} />
-      <label htmlFor="">password</label>
-      <input type="text" name="password" onChange={onChangeHandler} />
-      <button type="submit">submit</button>
-    </form>
+    <>
+      {showModal && <Modal />}
+      <form onSubmit={submitHandler}>
+        <label htmlFor="">name</label>
+        <input type="text" name="name" onChange={onChangeHandler} />
+        <label htmlFor="">password</label>
+        <input type="text" name="password" onChange={onChangeHandler} />
+        <button type="submit">submit</button>
+      </form>
+    </>
   );
 };
 
